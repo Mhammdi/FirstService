641adfbfc07fc1c78730c03e7b0428954a779c16
Mhammdi <mhammdi.khalil@gmail.com>
2019-12-29 13:52:18 +0100

    first commit

---
+++ src/main/java/com/vision/riad/FirstSericeApplication.java
@@ -0,0 +1,13 @@
+package com.vision.riad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSericeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSericeApplication.class, args);
+	}
+
+}
