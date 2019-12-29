b320205d977e3c32c28bbc5e5c9a9ace09bae748
Mhammdi <mhammdi.khalil@gmail.com>
2019-12-29 17:03:35 +0100

    third one

---
+++ src/main/java/com/vision/riad/model/User.java
@@ -0,0 +1,53 @@
+package com.vision.riad.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String nom;
+	private String login;
+	private String password;
+	
+	public User() {
+		this.id=0;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+	
+
+}
