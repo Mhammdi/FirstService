2ce18ae775723e1195d17df1231ba9410587785d
Mhammdi <mhammdi.khalil@gmail.com>
2019-12-30 20:09:37 +0100

    with db

--- src/main/java/com/vision/riad/dao/UserDB.java
+++ src/main/java/com/vision/riad/dao/UserDB.java
@@ -2,11 +2,10 @@ package com.vision.riad.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.vision.riad.model.User;
 
-@RepositoryRestResource(collectionResourceRel = "users",path="users")
+//@RepositoryRestResource(collectionResourceRel = "users",path="users")
 public interface UserDB extends JpaRepository<User,Integer>{
 
 }
